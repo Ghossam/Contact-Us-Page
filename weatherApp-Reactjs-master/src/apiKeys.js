9050320303887f73ef233ee4320287383726b123
Sanyam Kakkar <104959588+Ghossam@users.noreply.github.com>
2024-02-09 12:15:33 +0530

    Add files via upload
    Whole Weather App Code

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2985f5f172dab4d283f1c2e69f55c941",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
